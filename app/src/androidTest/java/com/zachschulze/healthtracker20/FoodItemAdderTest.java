ac3c089791e691fa55dd5852927ff53a510ef79c
Zach Schulze <zschulze@att.net>
2015-04-07 20:43:40 -0500

    Finished adding all test cases. Commented out the ones are impossible to test.

--- app/src/androidTest/java/com/zachschulze/healthtracker20/FoodItemAdderTest.java
+++ app/src/androidTest/java/com/zachschulze/healthtracker20/FoodItemAdderTest.java
@@ -1,7 +1,6 @@
 package com.zachschulze.healthtracker20;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import com.zachschulze.healthtracker20.database.FoodItemDataSource;
 import com.zachschulze.healthtracker20.models.FoodItem;
@@ -27,18 +26,165 @@ public class FoodItemAdderTest extends ActivityInstrumentationTestCase2<MainActi
         mFoodItemDataSource = new FoodItemDataSource(mMainActivity);
     }
 
-    @SmallTest
-    public void testFoodItemAdder1() {
+    public void testFoodItemAdder01() {
         FoodItem foodItemTest = new FoodItem("", -1, -1, "");
         boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
-        assertEquals(true, successful);
+        assertEquals(false, successful);
     }
 
-    public void testFoodItemAdder2() {
+    public void testFoodItemAdder02() {
         FoodItem foodItemTest = new FoodItem("Corn", 100, 1, "Cup");
         boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
         assertEquals(true, successful);
     }
+
+    public void testFoodItemAdder03() {
+        FoodItem foodItemTest = new FoodItem("", 100, 1, "Cup");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder04() {
+        FoodItem foodItemTest = new FoodItem("Corn", -1, -1, "Cup");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder05() {
+        FoodItem foodItemTest = new FoodItem("Broccoli", 150, 2, "Cups");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder06() {
+        FoodItem foodItemTest = new FoodItem("Frosted Flakes", 150, 3, "cups");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder07() {
+        FoodItem foodItemTest = new FoodItem("", 150, 3, "cups");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder08() {
+        FoodItem foodItemTest = new FoodItem("", -1, 3, "cups");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder09() {
+        FoodItem foodItemTest = new FoodItem("Frosted Flakes", 150, 3, "");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder10() {
+        FoodItem foodItemTest = new FoodItem("Steak", 850, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder11() {
+        FoodItem foodItemTest = new FoodItem("Steak", -1, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder12() {
+        FoodItem foodItemTest = new FoodItem("Water", 0, 1, "Liter");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder13() {
+        FoodItem foodItemTest = new FoodItem("Steak", 2147483647, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    /* Can't test an integer > 32 bits, if user inputs an integer > 32 bits the app will crash
+    public void testFoodItemAdder14() {
+        FoodItem foodItemTest = new FoodItem("Steak", 2147483648, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
+
+    /* In this case "XXX" represents a String > 32 bits, if user inputs a String > 32 bits
+        the app will crash
+    public void testFoodItemAdder15() {
+        FoodItem foodItemTest = new FoodItem("XXX", 100, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
+
+    /* In this case "XXXX" represents a String > 64 bits, if user inputs a String > 32 bits
+        the app will crash
+    public void testFoodItemAdder16() {
+        FoodItem foodItemTest = new FoodItem("XXXX", 100, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
+
+    /* In this case "XXXX" represents a int > 64 bits, if user inputs a String > 32 bits
+        the app will crash
+    public void testFoodItemAdder17() {
+        FoodItem foodItemTest = new FoodItem("Steak", XXXX, -1, "");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
+
+    /* Can't test decimal numbers as the user cannot enter decimal numbers
+    public void testFoodItemAdder18() {
+        FoodItem foodItemTest = new FoodItem("Steak", .000001, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
+
+    public void testFoodItemAdder19() {
+        FoodItem foodItemTest = new FoodItem("S", 850, 1, "Pound");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder20() {
+        FoodItem foodItemTest = new FoodItem("S", 1, 1, "g");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder21() {
+        FoodItem foodItemTest = new FoodItem("", -1, -1, "Cup");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(false, successful);
+    }
+
+    public void testFoodItemAdder22() {
+        FoodItem foodItemTest = new FoodItem("Water", 0, 0, "Liter");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    // In this case "XXX" represents a really long string that is < 32 bits
+    public void testFoodItemAdder23() {
+        FoodItem foodItemTest = new FoodItem("XXX", 100, 1, "Cup");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    public void testFoodItemAdder24() {
+        FoodItem foodItemTest = new FoodItem("100", 100, 100, "100");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }
+
+    /* User cannot input letters for calories and serving size attributes
+    public void testFoodItemAdder25() {
+        FoodItem foodItemTest = new FoodItem("Steak", "Steak", "Steak", "Steak");
+        boolean successful = mFoodItemDataSource.addFoodItem(foodItemTest);
+        assertEquals(true, successful);
+    }*/
 
     @Override
     protected void tearDown() throws Exception {
